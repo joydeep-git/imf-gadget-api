0201bcab35295dfa541e4d0d0de08358c76be1f5
joydeep-git <joydeep.accounts@tuta.io>
2025-04-26 00:00:37 +0530

    authRoutes and authControllers created

---
+++ src/routers/authRouters.ts
@@ -0,0 +1,36 @@
+import express, { Router } from "express";
+import authController from "../controllers/authControllers";
+import tokenValidator from "../middleware/tokenValidatorMiddleware";
+
+
+class AuthRouter {
+
+  public router: Router = express.Router();
+
+  constructor() {
+
+
+    this.router.post("/login", authController.signIn);
+
+
+    this.router.get("/details/:id", tokenValidator, authController.getDetails);
+
+
+    this.router.post("/register", authController.signUp);
+
+
+    this.router.delete("/logout/:id", tokenValidator, authController.signOut);
+
+
+    this.router.patch("/update/:id", tokenValidator, authController.updateUser);
+
+
+    this.router.delete("/delete/:id", tokenValidator, authController.deleteUser);
+
+  }
+
+}
+
+const authRouter = new AuthRouter();
+
+export default authRouter.router;
